7056172a4e23a3397f30a0ffc52aa0c2cc1f5e54
PENN JR <pennjunior680@gmail.com>
2023-06-08 16:38:21 +0100

    Adjusting some styles

--- src/app/Components/Organisms/Hero.tsx
+++ src/app/Components/Organisms/Hero.tsx
@@ -19,17 +19,17 @@ const Hero: NextPage<Props> = () => {
                 <img
                     alt=""
                     src='https://wallpapercave.com/uwp/uwp3789584.png'
-                    className='w-full h-[33rem] absolute'
+                    className='w-full h-[36rem] absolute'
                 />
-                <div className="inset-0 flex items-center justify-center p-24 py-6 h-screen md:h-[85vh] md:flex absolute left-">
+                <div className="inset-0 flex items-center justify-center p-16 py-6 h-screen md:h-[85vh] md:flex absolute">
                     <div className=" justify-items-center flex flex-col gap-7 md:max-w-md lg:max-w-none lg:justify-center absolute ">
-                        <h1 className=" text-center text-5xl text-white font-bold">IMAGINE A PLACE...</h1>
-                        <h2 className="  items-stretch text-white text-lg font-light tracking-wide lg:max-w-2xl w-full">
+                        <h1 className=" text-center text-5xl text-white font-black font-serif">IMAGINE A PLACE...</h1>
+                        <h2 className=" text-center items-stretch text-white text-lg font-light  tracking-wide lg:max-w-2xl w-full">
                             ...where you can belong to a school club, a gaming group,
                             or a worldwide art community. Where just you and a handful of friends can spend time together.
                             A place that makes it easy to talk every day and hang out more often.
                         </h2>
-                        <div className="flex flex-col sm:flex-row md:flex-col lg:flex-row md:items-start sm:items-center gap-6">
+                        <div className=" p-9 flex flex-col sm:flex-row md:flex-col lg:flex-row lg:items-csnter md:items-start sm:items-center gap-6 right-96">
                             <button className="bg-white w-60 font-medium flex items-center justify-center rounded-full p-4 text-lg hover:shadow-2xl hover:text-discord_blurple focus:outline-none transition duration-200 ease-in-out">
                                 <Download className="w-6 mr-2" />
                                 Download for Mac
