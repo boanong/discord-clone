2ad4299cafd3ae5148d65dc2ac21b27d7cea9f48
PENN JR <pennjunior680@gmail.com>
2023-06-14 14:50:38 +0100

    Adding some server pics

--- src/app/Components/Organisms/Sidebar.tsx
+++ src/app/Components/Organisms/Sidebar.tsx
@@ -1,5 +1,6 @@
 "use-client"
 
+import { Plus } from "react-feather";
 import ServerIcon from "./ServerIcon";
 
 /* eslint-disable @next/next/no-img-element */
@@ -15,18 +16,19 @@ export default function Sidebar() {
                 redirect('/login')
             } */}
             <div className="flex h-screen">
-                <div className="flex flex-col space-y-3 bg-[#202225] p-3 min-w-max">
+                <div className="flex flex-col space-y-4 bg-[#202225] p-3 min-w-max">
                     <div className="server-default hover:bg-discord_purple">
                         <img src="https://rb.gy/kuaslg" alt="" className="h-5" />
                     </div>
                     <hr className=" border-gray-700 border w-8 mx-auto" />
                     <ServerIcon image="https://rb.gy/qidcpp" />
-                    <ServerIcon image="https://rb.gy/zxo0lz" />
+                    <ServerIcon image="https://rb.gy/j5ocz" />
                     <ServerIcon image="https://rb.gy/lvwax" />
-                    <ServerIcon image="https://rb.gy/max6e" />
                     <ServerIcon image="https://rb.gy/jz089" />
-                    <ServerIcon image="https://rb.gy/8hleh" />
                     <ServerIcon image="https://rb.gy/eqel9" />
+                    <div className="server-default hover:bg-discord_green group">
+                        <Plus className="text-discord_green h-15 group-hover:text-white" />
+                    </div>
                 </div>
             </div>
         </>
